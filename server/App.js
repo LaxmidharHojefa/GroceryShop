60e9f5e6985f4e42282e917883dbb940e566ac3b
Hojefa Laxmidhar <laxmidharhuzaifa@gmail.com>
2026-01-28 17:45:45 +0530

    update app.js file to solve cors error

--- server/App.js
+++ server/App.js
@@ -11,12 +11,26 @@ const productRoutes = require("./src/routes/product.routes");
 
 connectDb(); 
 
+// NOTE: cors configuration for local development
+// app.use(cors({
+//   origin: 'http://localhost:5173',
+//   credentials: true,               // Required for cookies/sessions
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }));
+
+// NOTE: cors configuration for live server and vercel deployment
 app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true,               // Required for cookies/sessions
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  origin: [
+    "http://localhost:5173",
+    "https://grocery-shop-iota-tan.vercel.app",
+    "https://grocery-shop-backend-raba.onrender.com"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "public/images")));
